8154279c8f6a2897c73900c691fbcdf0fac48567
suafulk <zarucki@gmail.com>
2019-01-24 01:21:15 +0100

    Pieces and their movements.

--- src/main/scala/zarucki/chess/entities/Piece.scala
+++ src/main/scala/zarucki/chess/entities/Piece.scala
@@ -1,5 +1,144 @@
 package zarucki.chess.entities
 
-class Piece {
+// TODO: should pieces be distinguishable?
+// TODO: behaviour of pieces, how they move
+sealed trait Piece {
+	def representation: Char
 
+	override final def toString: String = {
+		representation.toUpper.toString
+	}
+
+	protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]]
+
+	protected def isMoveDestinationWithinBoard(addr: MaybeValidBoardAddress, maxFile: File, maxRank: Int): Boolean = {
+		addr.rank <= maxRank && addr.rank >= 0 && addr.fileAsInt <= maxFile.asInt && addr.fileAsInt >= 0
+	}
+
+	// TODO: rename this
+	// TODO: should this still be a stream? it is not infinite in nature!
+	def validMovesFor(startAddress: BoardAddress, maxFile: File, maxRank: Int): Stream[BoardAddress] = {
+		possibleMovesGenerators(startAddress)
+			.map(readValidBoardAddresses(_, maxFile, maxRank))
+  		.reduce(_ ++ _)
+	}
+
+	protected def readValidBoardAddresses(moveStream: Stream[MaybeValidBoardAddress], maxFile: File, maxRank: Int): Stream[BoardAddress] = {
+		moveStream
+			.filter(isMoveDestinationWithinBoard(_, maxFile, maxRank))
+			.map(maybeValidBoardAddress => BoardAddress(File(maybeValidBoardAddress.fileAsInt), maybeValidBoardAddress.rank))
+	}
+
+	protected type OneStepMovement = MaybeValidBoardAddress => MaybeValidBoardAddress
+
+	protected val toNorth: OneStepMovement = a => a.copy(rank = a.rank + 1)
+	protected val toEast: OneStepMovement = a => a.copy(fileAsInt = a.fileAsInt + 1)
+	protected val toSouth: OneStepMovement = a => a.copy(rank = a.rank - 1)
+	protected val toWest: OneStepMovement = a => a.copy(fileAsInt = a.fileAsInt - 1)
+
+	protected val toNorthEast = toNorth.andThen(toEast)
+	protected val toNorthWest = toNorth.andThen(toWest)
+	protected val toSouthEast = toSouth.andThen(toEast)
+	protected val toSouthWest = toSouth.andThen(toWest)
 }
+
+case class King() extends Piece {
+	override def representation: Char = 'K'
+
+	override protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]] = {
+		val mvba = MaybeValidBoardAddress(address)
+		List(
+			toNorth(mvba) #:: toNorthEast(mvba) #:: toEast(mvba) #::
+			toSouthEast(mvba) #:: toSouth(mvba) #:: toSouthWest(mvba) #::
+			toWest(mvba) #:: toNorthWest(mvba) #:: Stream.empty[MaybeValidBoardAddress]
+		)
+	}
+}
+
+case class Knight() extends Piece{
+	override def representation: Char = 'N'
+
+	override protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]] = {
+		val mvba = MaybeValidBoardAddress(address)
+		List(
+			toNorth.andThen(toNorth).andThen(toWest)(mvba) #::
+			toNorth.andThen(toNorth).andThen(toEast)(mvba) #::
+			toEast.andThen(toEast).andThen(toNorth)(mvba) #::
+			toEast.andThen(toEast).andThen(toSouth)(mvba) #::
+			toSouth.andThen(toSouth).andThen(toEast)(mvba) #::
+			toSouth.andThen(toSouth).andThen(toWest)(mvba) #::
+			toWest.andThen(toWest).andThen(toSouth)(mvba) #::
+			toWest.andThen(toWest).andThen(toNorth)(mvba) #::
+			Stream.empty[MaybeValidBoardAddress]
+		)
+	}
+}
+
+// TODO: not exactly sure this is most clear way to do this
+trait MovesManySquares {
+	self: Piece =>
+
+	protected def continousMovementInDirection(startAddress: BoardAddress, changeFun: OneStepMovement): Stream[MaybeValidBoardAddress] = {
+		lazy val moveStream: Stream[MaybeValidBoardAddress] = changeFun(MaybeValidBoardAddress(startAddress)) #:: moveStream.map(addr => changeFun(addr))
+		moveStream
+	}
+
+	// Streams for continous pieces are infinite, we need to read them til the first invalid move
+	override protected  def readValidBoardAddresses(moveStream: scala.Stream[MaybeValidBoardAddress], maxFile: File, maxRank: Int): scala.Stream[BoardAddress] = {
+		moveStream
+			.takeWhile(isMoveDestinationWithinBoard(_, maxFile, maxRank))
+			.map(maybeValidBoardAddress => BoardAddress(File(maybeValidBoardAddress.fileAsInt), maybeValidBoardAddress.rank))
+	}
+}
+
+case class Queen() extends Piece with MovesManySquares {
+	override def representation: Char = 'Q'
+
+	override protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]] = {
+		List(
+			continousMovementInDirection(address, toNorth),
+			continousMovementInDirection(address, toNorthEast),
+			continousMovementInDirection(address, toEast),
+			continousMovementInDirection(address, toSouthEast),
+			continousMovementInDirection(address, toSouth),
+			continousMovementInDirection(address, toSouthWest),
+			continousMovementInDirection(address, toWest),
+			continousMovementInDirection(address, toNorthWest)
+		)
+	}
+}
+
+case class Rook() extends Piece with MovesManySquares {
+	override def representation: Char = 'R'
+
+	override protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]] = {
+		List(
+			continousMovementInDirection(address, toNorth),
+			continousMovementInDirection(address, toEast),
+			continousMovementInDirection(address, toSouth),
+			continousMovementInDirection(address, toWest)
+		)
+	}
+}
+
+case class Bishop() extends Piece with MovesManySquares {
+	override def representation: Char = 'B'
+
+	override protected def possibleMovesGenerators(address: BoardAddress): List[Stream[MaybeValidBoardAddress]] = {
+		List(
+			continousMovementInDirection(address, toNorthEast),
+			continousMovementInDirection(address, toSouthEast),
+			continousMovementInDirection(address, toSouthWest),
+			continousMovementInDirection(address, toNorthWest)
+		)
+	}
+}
+
+object MaybeValidBoardAddress {
+	def apply(boardAddress: BoardAddress): MaybeValidBoardAddress = {
+		MaybeValidBoardAddress(fileAsInt = boardAddress.file.asInt, rank = boardAddress.rank)
+	}
+}
+
+// TODO: maybe instead of this introduce some NIL to BoardAddress
+case class MaybeValidBoardAddress(fileAsInt: Int, rank: Int)
---
+++ src/test/scala/zarucki/chess/entities/PieceSpec.scala
@@ -0,0 +1,150 @@
+package zarucki.chess.entities
+
+import zarucki.chess.UnitSpec
+
+class PieceSpec extends UnitSpec {
+	behavior of "Queen"
+
+	it should "properly move in all directions all the squares" in {
+		val queenValidMoves = Queen().validMovesFor(BoardAddress(File("C"), 2), maxFile = File("E"), maxRank = 4)
+		assert(queenValidMoves.toSet == Set(
+			BoardAddress(File("D"), 2), BoardAddress(File("E"), 2),
+			BoardAddress(File("B"), 2), BoardAddress(File("A"), 2),
+			BoardAddress(File("C"), 3), BoardAddress(File("C"), 4),
+			BoardAddress(File("C"), 1), BoardAddress(File("C"), 0),
+			BoardAddress(File("D"), 3), BoardAddress(File("E"), 4),
+			BoardAddress(File("B"), 1), BoardAddress(File("A"), 0),
+			BoardAddress(File("B"), 3), BoardAddress(File("A"), 4),
+			BoardAddress(File("D"), 1), BoardAddress(File("E"), 0)
+		))
+	}
+
+	it should "correctly handle being in the corner" in {
+		val queenValidMoves = Queen().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("C"), maxRank = 2)
+		assert(queenValidMoves.toSet == Set(
+			BoardAddress(File("A"), 1), BoardAddress(File("A"), 2),
+			BoardAddress(File("B"), 0), BoardAddress(File("C"), 0),
+			BoardAddress(File("B"), 1), BoardAddress(File("C"), 2)
+		))
+	}
+
+	it should "correctly handle 2 x 1 chess board" in {
+		val queenValidMoves = Queen().validMovesFor(BoardAddress(File("B"), 0), maxFile = File("B"), maxRank = 0)
+		assert(queenValidMoves.toSet == Set(BoardAddress(File("A"), 0)))
+	}
+
+	it should "correctly handle 1 x 1 chess board" in {
+		val queenValidMoves = Queen().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("A"), maxRank = 0)
+		assert(queenValidMoves.size == 0)
+	}
+
+	behavior of "King"
+
+	it should "properly move in all directions one square" in {
+		val kingValidMoves = King().validMovesFor(BoardAddress(File("C"), 2), maxFile = File("E"), maxRank = 4)
+		assert(kingValidMoves.toSet == Set(
+			BoardAddress(File("C"), 3),
+			BoardAddress(File("C"), 1),
+			BoardAddress(File("D"), 2),
+			BoardAddress(File("B"), 2),
+			BoardAddress(File("D"), 3),
+			BoardAddress(File("D"), 1),
+			BoardAddress(File("B"), 3),
+			BoardAddress(File("B"), 1)
+		))
+	}
+
+	it should "correctly handle being in the corner SW" in {
+		val kingValidMoves = King().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("C"), maxRank = 2)
+		assert(kingValidMoves.toSet == Set(BoardAddress(File("A"), 1), BoardAddress(File("B"), 0), BoardAddress(File("B"), 1)))
+	}
+
+	it should "correctly handle being in the corner SE" in {
+		val kingValidMoves = King().validMovesFor(BoardAddress(File("C"), 0), maxFile = File("C"), maxRank = 2)
+		assert(kingValidMoves.toSet == Set(BoardAddress(File("C"), 1), BoardAddress(File("B"), 0), BoardAddress(File("B"), 1)))
+	}
+
+	it should "correctly handle 2 x 1 chess board" in {
+		val kingValidMoves = King().validMovesFor(BoardAddress(File("B"), 0), maxFile = File("B"), maxRank = 0)
+		assert(kingValidMoves.toSet == Set(BoardAddress(File("A"), 0)))
+	}
+
+	it should "correctly handle 1 x 1 chess board" in {
+		val kingValidMoves = King().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("A"), maxRank = 0)
+		assert(kingValidMoves.size == 0)
+	}
+
+	behavior of "Bishop"
+
+	it should "properly move in NE, SE, SW, NW" in {
+		val bishopValidMoves = Bishop().validMovesFor(BoardAddress(File("C"), 2), maxFile = File("E"), maxRank = 4)
+		assert(bishopValidMoves.toSet == Set(
+			BoardAddress(File("D"), 3), BoardAddress(File("E"), 4),
+			BoardAddress(File("B"), 3), BoardAddress(File("A"), 4),
+			BoardAddress(File("D"), 1), BoardAddress(File("E"), 0),
+			BoardAddress(File("B"), 1), BoardAddress(File("A"), 0)
+		))
+	}
+
+	it should "correctly handle being in the corner" in {
+		val bishopValidMoves = Bishop().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("C"), maxRank = 2)
+		assert(bishopValidMoves.toSet == Set(BoardAddress(File("B"), 1), BoardAddress(File("C"), 2)))
+	}
+
+	it should "correctly handle 2 x 1 chess board" in {
+		val bishopValidMoves = Bishop().validMovesFor(BoardAddress(File("B"), 0), maxFile = File("B"), maxRank = 0)
+		assert(bishopValidMoves.size == 0)
+	}
+
+	behavior of "Rook"
+
+	it should "properly move in N, E, S, W" in {
+		val rookValidMoves = Rook().validMovesFor(BoardAddress(File("C"), 2), maxFile = File("E"), maxRank = 4)
+		assert(rookValidMoves.toSet == Set(
+			BoardAddress(File("C"), 3), BoardAddress(File("C"), 4),
+			BoardAddress(File("D"), 2), BoardAddress(File("E"), 2),
+			BoardAddress(File("C"), 1), BoardAddress(File("C"), 0),
+			BoardAddress(File("B"), 2), BoardAddress(File("A"), 2)
+		))
+	}
+
+	it should "correctly handle being in the corner" in {
+		val rookValidMoves = Rook().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("C"), maxRank = 2)
+		assert(rookValidMoves.toSet == Set(
+			BoardAddress(File("A"), 1), BoardAddress(File("A"), 2),
+			BoardAddress(File("B"), 0), BoardAddress(File("C"), 0))
+		)
+	}
+
+	it should "correctly handle 2 x 1 chess board" in {
+		val rookValidMoves = Rook().validMovesFor(BoardAddress(File("B"), 0), maxFile = File("B"), maxRank = 0)
+		assert(rookValidMoves.toSet == Set(BoardAddress(File("A"), 0)))
+	}
+
+	it should "correctly handle 1 x 1 chess board" in {
+		val rookValidMoves = Rook().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("A"), maxRank = 0)
+		assert(rookValidMoves.size == 0)
+	}
+
+	behavior of "Knight"
+
+	it should "properly move in horsey way" in {
+		val knightValidMoves = Knight().validMovesFor(BoardAddress(File("C"), 2), maxFile = File("E"), maxRank = 4)
+		assert(knightValidMoves.toSet == Set(
+			BoardAddress(File("D"), 4), BoardAddress(File("B"), 4),
+			BoardAddress(File("E"), 3), BoardAddress(File("E"), 1),
+			BoardAddress(File("D"), 0), BoardAddress(File("B"), 0),
+			BoardAddress(File("A"), 3), BoardAddress(File("A"), 1)
+		))
+	}
+
+	it should "correctly handle being in the corner" in {
+		val knightValidMoves = Knight().validMovesFor(BoardAddress(File("A"), 0), maxFile = File("C"), maxRank = 2)
+		assert(knightValidMoves.toSet == Set(BoardAddress(File("B"), 2), BoardAddress(File("C"), 1)))
+	}
+
+	it should "correctly handle 2 x 1 chess board" in {
+		val knightValidMoves = Knight().validMovesFor(BoardAddress(File("B"), 0), maxFile = File("B"), maxRank = 0)
+		assert(knightValidMoves.size == 0)
+	}
+}
